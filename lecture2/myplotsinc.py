07b045d0fe585d9a3771a9ef2b1eb47ac628aec1
Thomas Kamalakis <thkam@hua.gr>
2020-10-23 10:50:34 +0300

    added spectrum calculations in myplotsinc.py

--- lecture2/myplotsinc.py
+++ lecture2/myplotsinc.py
@@ -4,10 +4,17 @@ import matplotlib.pyplot as plt
 # Time axis 
 Npt = 1000          # number of points in the time axis
 T = 1e-9            # second
-T1 = 0.2e-9         # second
+T1 = 0.1e-9         # second
+
+# Frequency axis
+Npf = 1000
+Fmax = 40e9
 
 # Build time axis 
 t = np.linspace(-T/2, T/2, Npt )
+
+# Build frequency axis
+f = np.linspace(-Fmax, Fmax, Npf )
 
 # Calculate signal in the time domain
 x = np.zeros( t.size )
@@ -20,11 +27,17 @@ for tm in t:
     
     i += 1
 
+# signal spectrum
+X = T1 * np.sinc( f * T1 )
+
 # plot signal in the time domain
 plt.close('all')
 plt.figure(1)
 plt.plot(t, x)
 
+# plot signal spectrum 
+plt.figure(2)
+plt.plot(f, X)
         
     
 
